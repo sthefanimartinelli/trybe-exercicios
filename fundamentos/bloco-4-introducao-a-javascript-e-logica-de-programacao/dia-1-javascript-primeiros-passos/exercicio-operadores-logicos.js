b8537e40ded5f01eca7a3dfdc6f51bb7b8977b3f
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-09-21 15:09:25 -0300

    Alteração no exercício de operadores lógicos e criação de exercício para praticar switch-case

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-operadores-logicos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-operadores-logicos.js
@@ -13,4 +13,10 @@ if (currentHour >= 22) {
     message = "Hmmm, cheiro de café recém passado"
 }
 
-console.log(message);
+let weekDay = "quarta-feira";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else if (weekDay == "domingo" || weekDay == "sábado") {
+    console.log("FINALMENTE, descanso merecido UwU")
+}
