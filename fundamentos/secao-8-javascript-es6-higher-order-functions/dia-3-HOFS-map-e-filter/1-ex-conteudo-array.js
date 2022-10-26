eb42ccd72b6038df98ab1d4382ba73559c6a54ef
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-26 16:03:18 -0300

    Nova pasta criada para dia 3 de HOFs

---
+++ fundamentos/secao-8-javascript-es6-higher-order-functions/dia-3-HOFS-map-e-filter/1-ex-conteudo-array.js
@@ -0,0 +1,44 @@
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+  // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+// console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+// console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+// console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
+
+const reportStatus = (name, students) => {
+  const studentInfo = students.find((student) => student.nome === name);
+  return studentInfo.materias.map((materia) => (
+    `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+  ));
+};
+
+console.log(reportStatus('Natalia', estudantes));
