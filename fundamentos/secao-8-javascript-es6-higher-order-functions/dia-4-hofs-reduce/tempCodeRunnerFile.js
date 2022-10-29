8f30d8a2621991e4b4aa08660464f8b17feeb802
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-29 18:18:34 -0300

    Exercícios do conteúdo dia 8.4

--- fundamentos/secao-8-javascript-es6-higher-order-functions/dia-4-hofs-reduce/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const bigger = numbers.reduce((bigger, number) => {
-//   console.log(`valor do bigger é ${bigger} e o de number é ${number}`);
-//   return (bigger > number) ? bigger : number;
-// }, 0); 
-
-// console.log(bigger); // 85
